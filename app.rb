79adf2d912513f22237075ac2ab628ec4cc11037
Yurina Shin <yurinas@gmail.com>
2018-08-26 05:23:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     erb :display_puppy
   end
 
-  # post '/display_puppy' do
-  #   erb :display_puppy
-  # end
+  post '/display_puppy' do
+    erb :display_puppy
+  end
 end
