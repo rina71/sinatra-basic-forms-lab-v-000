07af48737c688cc8441ac17109ecf4e295c18e6d
Yurina Shin <yurinas@gmail.com>
2018-08-26 04:57:52 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(title, breed, age)
-    @title = title, @breed = breed, @age = age
+  def initialize(name, breed, age)
+    @name = name, @breed = breed, @age = age
   end
 end
